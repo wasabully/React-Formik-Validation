ff355d858d38b2eb04b2035cfe59bf295f7b04ac
danil <danildemchenko.official@gmail.com>
2024-07-22 15:24:36 +0300

    done

---
+++ src/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { CustomForm } from './components/formik/CustomForm';
+
+function App() {
+	return (
+		<section>
+			<CustomForm />
+		</section>
+	);
+}
+
+export default App;
